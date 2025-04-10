84d127988ff1a0c8cb86c81781454accb1a9f39e
Adarsh singh <162829540+Adarshsingh99@users.noreply.github.com>
2025-04-10 10:33:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if array contains duplicates/check-if-array-contains-duplicates.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int prices[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                prices[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            if (obj.checkDuplicates(prices)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean checkDuplicates(int arr[]) {
+        // Code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        for (int i=0;i<n-1;i++)
+        {
+            if (arr[i]==arr[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
