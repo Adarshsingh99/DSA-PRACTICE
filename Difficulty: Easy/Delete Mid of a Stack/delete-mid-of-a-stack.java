08d2fead245c80f90009e0e001be8cb9cfba0b31
Adarsh singh <162829540+Adarshsingh99@users.noreply.github.com>
2026-02-23 19:03:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.java
+++ Difficulty: Easy/Delete Mid of a Stack/delete-mid-of-a-stack.java
@@ -1,89 +1,19 @@
-//{ Driver Code Starts
-// Initial template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine();
-        while (t-- > 0) {
-            String temp[] = sc.nextLine().trim().split(" ");
-            Stack<Integer> myStack = new Stack<>();
-            int n = temp.length;
-            // adding elements to the stack
-            for (int i = 0; i < n; i++) {
-                int x = Integer.parseInt(temp[i]);
-                myStack.push(x);
-            }
-            Solution obj = new Solution();
-            obj.deleteMid(myStack);
-
-            while (!myStack.isEmpty()) {
-                System.out.print(myStack.peek() + " ");
-                myStack.pop();
-            }
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    
-//  static void insertAtlast(Stack<Integer> st , int element)
-//     {
-//         if(st.isEmpty())
-//         {
-//             st.push(element);
-//             return;
-//         }
-        
-//         int temp = st.pop();
-//         insertAtlast(st,element);
-//         st.push(temp);
-//       }
-    
-    // function for reverseing stack
-    // static void reverse(Stack<Integer> st)
-    // {
-    //     if(st.isEmpty())
-    //     {
-    //         return;
-    //     }
-        
-        
-    //     int temp = st.pop();
-    //     reverse(st);
-    //     insertAtlast(st,temp);
-    // }
-    
-    static void solve(Stack<Integer> s , int middle)
-    {
-        if(s.size()==middle)
-        {
+
+    public void deleteMid(Stack<Integer> s) {
+        int n = s.size();
+        int k = (n / 2) + 1;  // correct middle position (1-based)
+        solve(s, k);
+    }
+
+    public void solve(Stack<Integer> s, int k) {
+        if(k == 1) {
             s.pop();
             return;
         }
-        
-        
+
         int temp = s.pop();
-        solve(s,middle);
+        solve(s, k - 1);
         s.push(temp);
-        
-    }
-    // Function to delete middle element of a stack.
-    public void deleteMid(Stack<Integer> s) {
-        // code here
-        int middle = (s.size()+1)/2;
-        solve(s,middle);
-        // reverse(s);
     }
 }
