a0cb7646b2f73e63355392e60a7178f1dc649a76
Adarsh singh <162829540+Adarshsingh99@users.noreply.github.com>
2026-02-25 20:29:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,44 +1,36 @@
 class Solution {
     public int aggressiveCows(int[] stalls, int k) {
         // code here
- 
-       Arrays.sort(stalls);
-       
-       int start = 1;
-       int end = stalls[stalls.length-1];
-       
-       while(start<=end)
-       {
-           int mid = (start+end)/2;
-           if(possible(stalls,mid,k))
-           {
-               start=mid+1;
-           }
-           else
-           {
-               end=mid-1;
-           }
-       }
-       return  end;
-       
-    }
-    
-    public boolean possible(int []stalls, int mini, int k)
-    {
-        int prev = stalls[0];
-        int cow = 1;
-        
-        for(int i =1 ;i<stalls.length;i++)
+        Arrays.sort(stalls);
+        int n = stalls.length;
+        int left = 1;
+        int right = stalls[n-1]-stalls[0];
+        while(left <=right)
         {
-            if(stalls[i]-prev >=mini)
+            int mid = (left+right)/2;
+            if(possible(stalls, k , mid))
+            {
+                left = mid+1;
+            }
+            else 
+            {
+                right = mid-1;
+            }
+        }
+        return left-1;
+    }
+    public Boolean possible(int arr[] , int k ,int dist)
+    {
+        int cow = 1;
+        int lastpos = arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]-lastpos >= dist)
             {
                 cow++;
-                prev=stalls[i];
+                lastpos = arr[i];
             }
-            
-            if(cow>=k) return true;
         }
-        
-        return false;
+        return cow>=k;
     }
 }
