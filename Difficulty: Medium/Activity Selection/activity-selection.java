fed62d271123e2e83c9fa4e26d5f50bf642b5a78
Adarsh singh <162829540+Adarshsingh99@users.noreply.github.com>
2026-03-18 20:20:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -0,0 +1,37 @@
+class Solution {
+     public class pair
+    {
+        int start;
+        int end;
+        pair(int s, int e)
+        {
+            start = s;
+            end = e;
+        }
+    }
+    public int activitySelection(int[] start, int[] end) {
+        // code here
+                int n = start.length;
+        
+        pair [] meeting = new pair[n];
+        for(int i=0;i<n ;i++)
+        {
+            meeting[i] = new pair(start[i] , end[i]);
+        }
+        
+        // sort by end of time
+        Arrays.sort(meeting , (a, b)->a.end - b.end);
+        
+        int ans =1;
+        int prevEnd = meeting[0].end;
+        for(int i=1;i<n ;i++)
+        {
+            if(meeting[i].start > prevEnd)
+            {
+                ans++;
+                prevEnd = meeting[i].end;
+            }
+        }
+        return ans;
+    }
+}
