96761ab6a2490127596ea2507af1dfc799080d7b
Adarsh singh <162829540+Adarshsingh99@users.noreply.github.com>
2026-03-19 11:35:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.java
@@ -0,0 +1,32 @@
+class Solution {
+    static int minDeletions(String s) {
+        // code here
+    return s.length()-longestPalindromeSubseq(s);
+    }
+        public static int longestPalindromeSubseq(String s) {
+          String s1 =  new StringBuilder(s).reverse().toString();
+       return longestCommonSubsequence(s, s1);
+    }
+        public static int longestCommonSubsequence(String text1, String text2) {
+        int m= text1.length();
+        int n = text2.length();
+        int t[][]=new int [m+1][n+1];
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0 || j==0) t[i][j]=0;
+                else if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else
+                {
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+    
+}
